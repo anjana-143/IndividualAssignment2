f7dbe116e6972796fdf8f6904c7a20dc92974b1c
Anjana Shrestha <anjena456@gmail.com>
2022-02-17 12:57:46 +0545

    Initial commit

---
+++ src/Integer_Answer2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Integer_Answer2 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number : ");
+        int a = in.nextInt();
+        System.out.print("Input the second number: ");
+        int b = in.nextInt();
+        System.out.println("Result: "+result(a, b));
+    }
+    public static int result(int x, int y)
+    {
+        if(x == y)
+            return 0;
+        if(x % 7 == y % 7)
+            return Math.min(x, y);
+        return Math.max(x, y);
+    }
+}
+
+
+
